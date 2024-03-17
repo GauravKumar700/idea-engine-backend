6fc94e3207dcac42be2d71ff830e914ec9061d82
Gaurav Kumar <gauravkumar7004515@gmail.com>
2024-03-17 12:34:29 +0530

    handle promise

--- db.js
+++ db.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_URI).then((data) => {
         console.log(`Mongodb Database Connected`);
-    });
+    }).then(() => {}).catch((e) => {
+        console.log(e)
+    })
 }
 
 module.exports = connectDatabase; 
