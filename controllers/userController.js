17e01fa28865300ce70ec3710fed24538791f61e
Gaurav Kumar <gauravkumar7004515@gmail.com>
2024-04-26 19:12:24 +0530

    Version 1

--- controllers/userController.js
+++ controllers/userController.js
@@ -69,35 +69,43 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
 
 const genAI = new GoogleGenerativeAI("AIzaSyAM1T6li4pgjil1q55wbC_UvYq-cbNJs2I");
 const answer = async (question) => {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const prompt = question
-    const result = await model.generateContent(prompt);
-    // const response = await result.response;
-    const res = result.response;
-    const text = res.text();
-    // console.log(text);
-    return text
+    try {
+        // For text-only input, use the gemini-pro model
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        const prompt = question
+        const result = await model.generateContent(prompt);
+        // const response = await result.response;
+        const res = result.response;
+        const text = res.text();
+        // console.log(text);
+        return text
+    } catch (error) {
+        window.alert(error)
+    }
 }
 
 const generatePrompt = async (questions, answers) => {
-    let buisnessName = answers[questions.indexOf('business name')]
-    let buisnessStage = answers[questions.indexOf('business stage')]
-    let buisnessDescription = answers[questions.indexOf('business description')]
-    let numberOfEmployees = answers[questions.indexOf('Number of employees')]
-    let buisnessOffers = answers[questions.indexOf('Do you offer a product or service?')]
-    let customerGetMode = answers[questions.indexOf('How can customer get your product or service?')]
-    let regionOfService = answers[questions.indexOf('Where do you serve your customers?')]
-    let productServiceName = answers[questions.indexOf('Product or Service Name')]
-    let productServiceDescription = answers[questions.indexOf('Product or Service Description')]
-    let intialInvestment = answers[questions.indexOf('Total Initial Investment')]
-    let firstYearRevenue = answers[questions.indexOf('Expected First Year Revenue')]
-    let rateOfGrowthPerYear = answers[questions.indexOf('How much do you expect your revenue to grow each year?')]
-    let yearlyBuisnessOperatingCost = answers[questions.indexOf('Yearly Business Operations Cost')]
+    try {
+        let buisnessName = answers[questions.indexOf('business name')]
+        let buisnessStage = answers[questions.indexOf('business stage')]
+        let buisnessDescription = answers[questions.indexOf('business description')]
+        let numberOfEmployees = answers[questions.indexOf('Number of employees')]
+        let buisnessOffers = answers[questions.indexOf('Do you offer a product or service?')]
+        let customerGetMode = answers[questions.indexOf('How can customer get your product or service?')]
+        let regionOfService = answers[questions.indexOf('Where do you serve your customers?')]
+        let productServiceName = answers[questions.indexOf('Product or Service Name')]
+        let productServiceDescription = answers[questions.indexOf('Product or Service Description')]
+        let intialInvestment = answers[questions.indexOf('Total Initial Investment')]
+        let firstYearRevenue = answers[questions.indexOf('Expected First Year Revenue')]
+        let rateOfGrowthPerYear = answers[questions.indexOf('How much do you expect your revenue to grow each year?')]
+        let yearlyBuisnessOperatingCost = answers[questions.indexOf('Yearly Business Operations Cost')]
 
-    let prompt = `Create an end to end business plan as well as guidance plan that helps me create a business by the name of ${buisnessName}.It is in the stage of ${buisnessStage}.${buisnessName} is suppose to be ${buisnessDescription}.At this stage our comapny contains ${numberOfEmployees} number of employees. ${buisnessName} offers ${buisnessOffers}. A customer gets my ${buisnessOffers} from ${customerGetMode} mode. We Serve our customers in ${regionOfService}. Our ${buisnessOffers} name is ${productServiceName}. ${productServiceName} is suppose to be ${productServiceDescription}. Our Intial investment is ${intialInvestment}.Our expected first year revenue is ${firstYearRevenue} And i want to grow my revenue at the rate of ${rateOfGrowthPerYear} Our buisness yearly operating buisness cost is ${yearlyBuisnessOperatingCost}.`
+        let prompt = `Create an end to end business plan as well as guidance plan that helps me create a business by the name of ${buisnessName}.It is in the stage of ${buisnessStage}.${buisnessName} is suppose to be ${buisnessDescription}.At this stage our comapny contains ${numberOfEmployees} number of employees. ${buisnessName} offers ${buisnessOffers}. A customer gets my ${buisnessOffers} from ${customerGetMode} mode. We Serve our customers in ${regionOfService}. Our ${buisnessOffers} name is ${productServiceName}. ${productServiceName} is suppose to be ${productServiceDescription}. Our Intial investment is ${intialInvestment}.Our expected first year revenue is ${firstYearRevenue} And i want to grow my revenue at the rate of ${rateOfGrowthPerYear} Our buisness yearly operating buisness cost is ${yearlyBuisnessOperatingCost}.`
 
-    return prompt
+        return prompt
+    } catch (error) {
+        window.alert(error)
+    }
 }
 
 // Creating PDF
